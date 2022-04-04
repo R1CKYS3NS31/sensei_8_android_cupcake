449f6cdd7cd2a3212ba5eef3a79135d150a2f15b
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-04-04 13:16:33 +0300

    send the order commit

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -16,7 +16,6 @@
 package com.example.cupcake
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
